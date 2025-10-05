295958078bff09713a86a9f0ab2f44e4939ba97d
REAM.KHORN <REAM.KHORN@PNC.LOCAL>
2025-10-05 10:52:02 +0700

    add

---
+++ main/main.js
@@ -0,0 +1,11 @@
+ const mobileToggle = document.getElementById('mobileToggle');
+        const navMenu = document.getElementById('navMenu');
+
+        mobileToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
+        document.querySelectorAll('.nav-link').forEach(link => {
+            link.addEventListener('click', () => {
+                navMenu.classList.remove('active');
+            });
+        });
